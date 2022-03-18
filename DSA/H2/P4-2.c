d0c6725977925f3c41a2607f93d77be1f392adcc
CH <chyang@getois.com>
2022-03-18 18:16:21 +0800

    first rev

--- DSA/H2/P4-2.c
+++ DSA/H2/P4-2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #define MAX_OP_NUM  9
-#define MAXLEN      1000
+#define MAXLEN      100000
 
 typedef struct _entry {
     int op_no;
@@ -33,7 +33,7 @@ void pop(ENTRY *e, int *op_no, long long int *num)
     *num = e->num ;
     *op_no = e->op_no ;
 }
-long long int do_op (int op_no,long long int num,long long num2)
+long long int do_op (int op_no,long long int num, long long int num2)
 {
     long long int ret;
     switch (op_char[op_no])
@@ -65,16 +65,13 @@ long long int calculate (ENTRY in[], int in_top, ENTRY stack[], int s_top)
     int intop = in_top;
     int stop = s_top;
     long long int num, num2;
-    int op_no;
-
-  //  for (int i=0;i<=in_top;i++) 
-  //      printf("[%c:%lld]\n",op_char[in[i].op_no],in[i].num);
+    int op_no, tmp;
 
     for (int i=0;i<=in_top;i++) {
         pop (&in[i], &op_no, &num);
         if (op_no) {
-            pop (&stack[stop--], &op_no, &num2);
-            pop (&stack[stop--], &op_no, &num);
+            pop (&stack[stop--], &tmp, &num2);
+            pop (&stack[stop--], &tmp, &num);            
             num = do_op (op_no, num, num2);
             push ( &stack[++stop], 0, num);
         } else {
@@ -95,13 +92,14 @@ void main()
  
     while ((c=getchar())!=EOF) {
         if (isdigit(c)) {
-            if (prev_mode=OP_MODE) 
-                temp=0;
             temp = temp * 10 + c - '0';
             prev_mode = NUM_MODE;
         } else {
-            if (prev_mode== NUM_MODE) 
+            if (prev_mode== NUM_MODE) {
+//printf("push num=%lld\n",temp);
                 push(&out[++out_top], 0, temp);
+                temp = 0;
+            }
             prev_mode=OP_MODE;    
             cop_no = get_op_no(c);    
             switch (c) {
@@ -117,7 +115,6 @@ void main()
                 case '-': 
                 case '+': 
                 case '(':
-                case '=': 
                     while ((s_top>=0) && (op_prio[cop_no] <= op_prio[stack[s_top].op_no]) && (op_prio[cop_no]>0)) {
                         // pop stack and push into out
                         push(&out[++out_top], stack[s_top].op_no, stack[s_top].num);
@@ -127,25 +124,21 @@ void main()
                     s_top++;
                     stack[s_top].op_no = cop_no;
                     stack[s_top].num = 0;
-                    
-                    if (c =='=') {
-                        //dry out all op in stack and calculate
-                        while (s_top>=0) {
-                            // pop stack and push into out
-                            push(&out[++out_top], stack[s_top].op_no, stack[s_top].num);
-                            s_top--;  
-                        }  
-                        
-                        printf("in_top=%d, s_top=%d\n",out_top,s_top);
-                        num = calculate(out, out_top, stack, s_top);  //TODO
-                        printf("Print: %lld\n",num);
-                    } 
+                    break;
+
+                case '=':    
+                    //dry out all op in stack and calculate
+                    while (s_top>=0) {
+                        // pop stack and push into out
+                        push(&out[++out_top], stack[s_top].op_no, stack[s_top].num);
+                        s_top--;  
+                    }  
+                    num = calculate(out, out_top, stack, s_top);  
+                    printf("Print: %lld\n",num);                    
                     break;    
                 default:
                     break;    
             } 
         }   
     }
-    //postfix to cal
- 
 }
