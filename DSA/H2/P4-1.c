d4e2d208bd4f2b7a9bd0ea0d20320431b108ac40
CH <chyang@getois.com>
2022-03-18 11:43:05 +0800

    update

--- DSA/H2/P4-1.c
+++ DSA/H2/P4-1.c
@@ -1,39 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define MAX_OP_NUM  9
+#define MAXLEN      1000000
 
 typedef struct _entry {
-    int num;
-    char op;
+    int op;
+    long long int num;
 } ENTRY;
 
-typedef struct _stack {
-    ENTRY s[10000];
-    int top=-1;
-} STACK;
+ENTRY S[100000];
 
-STACK store, out;
-
-ENTRY* pop (STACK *s)
+char pri[MAX_OP_NUM] = { '=', '(', ')', ' ', '+', '-', '*', '/', '%' };
+int priority(char in, char stack)
 {
-    ENTRY *ret;
+    int pi, ps;
 
-    if (s->top>=0) {
-        ret=s->
-        gTop--;
-    } else
-        s=NULL;
+    if ((in=='(') || (stack=='('))
+        return 0;
+
+    for (int i=0; i<MAX_OP_NUM;i++) {
+        if (in == pri[i])
+            pi=i;
+        if (stack == pri[i])
+            ps=i;    
+    }
+    if (pi/3 <= ps/3)
+        return 1;
+    return 0;    
 }
-
-ENTRY push(ENTRY *s) 
+#define OP_MODE  0
+#define NUM_MODE 1
+void main()
 {
-    gTop++;
-    gSTACK[gTop].num = s->num;
-    gSTACK[gTop].op = s->op;
-}
+    int top=-1, ctr;
+    char stack[MAXLEN],out[MAXLEN];
+    int  s_top=-1, out_top=-1;
+    char c;
+    int temp, prev_mode=OP_MODE;
 
-int main()
-{
-
-
+    while ((c=getchar())!='\n') {
+        if (isdigit(c)) {
+            if (prev_mode=OP_MODE) {
+                temp=0;
+            }
+            temp = temp * 10 + c - '0';
+            prev_mode = NUM_MODE;
+        } else {
+            if (prev_mode== NUM_MODE) {
+         //       push(temp)
+            }
+            switch (c) {
+                case ')':  //dump stack to out until ( found
+                    while (stack[s_top]!= '(') 
+     //push op                   out[++out_top] = stack[s_top--]; 
+                    s_top--;      
+                    break;
+                case '%': 
+                case '/': 
+                case '*':
+                case '-': 
+                case '+': 
+                case '(':
+                case '=': 
+                    while ((s_top>=0) && (priority(c, stack[s_top]))) {
+      //push op                  out[++out_top]=stack[s_top--];
+                    }
+                    stack[++s_top]=c;
+                    if (c=='=')
+      //push                out[++out_top]=stack[s_top--]; 
+                    break;    
+                default:
+                    break;    
+            }    
+    }
+    //postfix to cal
+ 
 }
--- DSA/H2/P5.c
+++ DSA/H2/P5.c
@@ -10,45 +10,67 @@ typedef struct _USB {
 } USB;
 
 typedef struct _ROOM {
-    long long int room_no;
     int state;  //open(1) or close(0)
     USB *first;
     USB *last;
 } ROOM;
 
-int Room_add_user(ROOM *r, USB *u){
-    
-    //if r->first 不存在
-        // init r->first = r->last = u
-        // return
+void Room_add_user(ROOM *r, USB *u)
+{
+    USB *ptr, *prev_ptr ;
+    int gmatch = 0;
 
-    //Get user from r->first, loop 
-    // Loop 
-        // if user-> group, group_match = 1
-        // if group_match == 1,
-            // if find user->group != group. 
-                // append the USB before the first unmatch
-
-    // if loop until tail, no found
-    // Add USB into the tail, update r->last        
+    if (r->first == NULL) {
+        r->first = r->last = u;
+        return;
+    }
+    ptr=r->first;
+    while (ptr != NULL) {
+        if (ptr->group==u->group) 
+            gmatch=1;
+        if ((ptr->group!=u->group)&&(gmatch==1)) {
+            prev_ptr->next = ptr->prev = u;
+            u->next = ptr;
+            u->prev = prev_ptr;
+            return;
+        }
+        prev_ptr = ptr;
+        ptr = ptr->next;
+    }
+    prev_ptr->next = r->last = u;
+    u->next = NULL;
 }
 
-int Room_leave_user(ROOM *r, USB *u)
+USB *Room_leave_user(ROOM *r)
 {
-    // Get User u from r->last
-     
-    // Change the second last USB's next to null
-    // Change r->last
+    USB *ptr, *prev_ptr;
     
+    ptr = r->last;
+    prev_ptr = ptr->prev;
+
+    if (prev_ptr) {
+        prev_ptr->next = NULL;
+        r->last = prev_ptr;
+    } else {
+        r->last = r->first = NULL;
+    }
+    return ptr;
 }
 
-int Room_go_user(ROOM *r, USB *u)
+USB *Room_go_user(ROOM *r)
 {
-    // Get User u from r->first
-     
-    // Change the second USB's prev to null
-    // Change r->first
+    USB *ptr, *next_ptr;
     
+    ptr = r->last;
+    next_ptr = ptr->next;
+
+    if (next_ptr) {
+        next_ptr->prev = NULL;
+        r->first = next_ptr;
+    } else {
+        r->last = r->first = NULL;
+    }
+    return ptr;
 }
 
 int Room_close(ROOM *r, ROOM *r1)
@@ -61,65 +83,63 @@ int Room_close(ROOM *r, ROOM *r1)
     // Mark Room r->state = 0  // closed
 }
 
+void Room_print(ROOM *r)
+{
+
+
+}
+
 int main()
 {
     int M,N,K;
     char cmd[32];
-    int i,j,m;
+    int i, j, m, loop;
+    USB *u;
 
     scanf("%d%d%d",&M,&N,&K);  //Room, Lines and Group. 
 
-    ROOM BRoom[M];  //
-    for (int in=0; in< N; in++) {
+    ROOM BRoom[M];  
+    for (loop=0; loop< M; loop++) {
+        BRoom[loop].first=BRoom[loop].last=NULL;
+        BRoom[loop].state=1;
+    }
+
+    for (loop=0; loop< N; loop++) {
         scanf("%s",cmd);
 
         if (!strcmp(cmd,"enter")) {
             scanf("%d%d%d",&i,&j,&m);
-            printf("repeat:enter %d %d %d\n",i,j,m);
+            //printf("repeat:enter %d %d %d\n",i,j,m);
+            u = Init_USB(i,j);
+            Room_add_user(&BRoom[m], u);
         }
 
         if (!strcmp(cmd,"leave")) {
             scanf("%d",&m);
-            printf("repeat:leave %d\n",m);
+            //printf("repeat:leave %d\n",m);
+            Room_leave_user(&BRoom[m]);
         }
 
         if (!strcmp(cmd,"go")) {
             scanf("%d",&m);
-            printf("repeat:go %d\n",m);
+            //printf("repeat:go %d\n",m);
+            Room_go_user(&BRoom[m]);
         }
+
         if (!strcmp(cmd,"close")) {
             scanf("%d",&m);
-            printf("repeat:close %d\n",m);
+            //printf("repeat:close %d\n",m);
+            for (int step=1; step < M/2; step++) {
+                if (BRoom[(M+m-step)%M].state==1) {
+                    Room_close(m, (M+m-step)%M);
+                    break;
+                } else if (BRoom[(M+m+step)%M].state==1) {
+                    Room_close(m, (M+m+step)%M);
+                    break;
+                } 
+            }
         }
     }
-
-    //scanf M,N,K
-    // Init Room stucture (M). 
-
-    // scanf N line
-    //Loop scan command
-        //if command = "Enter" then
-            // scan Id, Group, Room 
-            // Init a USB structure and fill ID and Group
-            // Append USB to Room Room_add_user()
-
-        //if command = "Leave"       
-            // scan Room (r)
-            // Find and Split the last user by Room_leave_user(ROOM *r, USB *u)
-            // Free u
-
-        //if command = "Go"
-            // scan Room (r)
-            // Find and Split the first user by Room_Go_user(ROOM *r, USB *u)  
-            // Free u
-            
-        // if command = "close"
-            // scan Room (r)
-            // Find the nearest smaller open room_no (r1)
-            // Room_Close(r, r1)
-
-    // Output resule
-    //  Loop Room (0 to M-1)
-        // Print USB
-
+    for (loop=0;loop<M;loop++)
+        Room_print(&BRoom[loop]);
 }
