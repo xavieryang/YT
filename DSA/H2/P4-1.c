845d4189496201693af7f0dcff9cce2cc7f59990
CH <chyang@getois.com>
2022-03-18 17:29:00 +0800

    first draft

--- DSA/H2/P4-1.c
+++ DSA/H2/P4-1.c
@@ -2,59 +2,116 @@
 #include <stdlib.h>
 #include <ctype.h>
 #define MAX_OP_NUM  9
-#define MAXLEN      1000000
+#define MAXLEN      10000
 
 typedef struct _entry {
-    int op;
+    int op_no;
     long long int num;
 } ENTRY;
 
-ENTRY S[100000];
+char op_char[MAX_OP_NUM] =  { ' ' , '(' , ')' , '=' , '+' , '-' , '*' , '/' , '%' };
+int  op_prio[MAX_OP_NUM] =  { -1  , -1  , -1  ,  -1 ,  1  ,  1  ,  2  ,  2  ,  2  };
 
-char pri[MAX_OP_NUM] = { '=', '(', ')', ' ', '+', '-', '*', '/', '%' };
-int priority(char in, char stack)
-{
-    int pi, ps;
-
-    if ((in=='(') || (stack=='('))
-        return 0;
-
-    for (int i=0; i<MAX_OP_NUM;i++) {
-        if (in == pri[i])
-            pi=i;
-        if (stack == pri[i])
-            ps=i;    
-    }
-    if (pi/3 <= ps/3)
-        return 1;
-    return 0;    
-}
 #define OP_MODE  0
 #define NUM_MODE 1
+
+int get_op_no(char c)
+{
+    for (int i=0;i<MAX_OP_NUM;i++)
+        if (c == op_char[i])
+            return i;
+}  
+
+void push(ENTRY *e, int op_no, long long int num)
+{
+    e->num = num;
+    e->op_no = op_no;
+}
+
+void pop(ENTRY *e, int *op_no, long long int *num)
+{
+    *num = e->num ;
+    *op_no = e->op_no ;
+}
+long long int do_op (int op_no,long long int num,long long num2)
+{
+    long long int ret;
+    switch (op_char[op_no])
+    {
+    case '+'/* constant-expression */:
+        ret = num + num2;
+        break;
+    case '-'/* constant-expression */:
+        ret = num - num2;
+        break;
+    case '*'/* constant-expression */:
+        ret = num * num2;
+        break;
+    case '/'/* constant-expression */:
+        ret = num /num2;
+        break;
+    case '%'/* constant-expression */:
+        ret = num % num2;
+        break;
+    
+    default:
+        break;
+    }
+    return ret;
+}
+
+long long int calculate (ENTRY in[], int in_top, ENTRY stack[], int s_top)
+{
+    int intop = in_top;
+    int stop = s_top;
+    long long int num, num2;
+    int op_no;
+
+    printf("in_top=%d, s_top=%d\n",in_top,s_top);
+
+    for (int i=0;i<=in_top;i++) 
+        printf("~[%c:%lld]\n",op_char[in[i].op_no],in[i].num);
+
+    for (int i=0;i<=in_top;i++) {
+        pop (&in[i], &op_no, &num);
+        if (op_no) {
+            pop (&stack[stop--], &op_no, &num2);
+            pop (&stack[stop--], &op_no, &num);
+            num = do_op (op_no, num, num2);
+            push ( &stack[++stop], 0, num);
+        } else {
+            push ( &stack[++stop], 0, num);
+        }
+    }
+    pop(&stack[stop--], &op_no, &num);
+    return num;
+}
+
 void main()
 {
-    int top=-1, ctr;
-    char stack[MAXLEN],out[MAXLEN];
+    ENTRY stack[10000], out[MAXLEN];
     int  s_top=-1, out_top=-1;
     char c;
-    int temp, prev_mode=OP_MODE;
-
+    long long int num, temp=0; 
+    int prev_mode=OP_MODE, cop_no;
+ 
     while ((c=getchar())!='\n') {
         if (isdigit(c)) {
-            if (prev_mode=OP_MODE) {
+            if (prev_mode=OP_MODE) 
                 temp=0;
-            }
             temp = temp * 10 + c - '0';
             prev_mode = NUM_MODE;
         } else {
-            if (prev_mode== NUM_MODE) {
-         //       push(temp)
-            }
+            if (prev_mode== NUM_MODE) 
+                push(&out[++out_top], 0, temp);
+            prev_mode=OP_MODE;    
+            cop_no = get_op_no(c);    
             switch (c) {
                 case ')':  //dump stack to out until ( found
-                    while (stack[s_top]!= '(') 
-     //push op                   out[++out_top] = stack[s_top--]; 
-                    s_top--;      
+                    while (op_char[stack[s_top].op_no] != '(')
+                        push(&out[++out_top], stack[s_top--].op_no, 0);
+                    // decrease s_top to drop '(' from the stack
+                    s_top--;     
                     break;
                 case '%': 
                 case '/': 
@@ -63,16 +120,31 @@ void main()
                 case '+': 
                 case '(':
                 case '=': 
-                    while ((s_top>=0) && (priority(c, stack[s_top]))) {
-      //push op                  out[++out_top]=stack[s_top--];
+                    while ((s_top>=0) && (op_prio[cop_no] <= op_prio[stack[s_top].op_no]) && (op_prio[cop_no]>0)) {
+                        // pop stack and push into out
+                        push(&out[++out_top], stack[s_top].op_no, stack[s_top].num);
+                        s_top--;  
                     }
-                    stack[++s_top]=c;
-                    if (c=='=')
-      //push                out[++out_top]=stack[s_top--]; 
+                    //push c into stack
+                    s_top++;
+                    stack[s_top].op_no = cop_no;
+                    stack[s_top].num = 0;
+                    
+                    if (c=='=') {
+                        //dry out all op in stack and calculate
+                        while (s_top>=0) {
+                            // pop stack and push into out
+                            push(&out[++out_top], stack[s_top].op_no, stack[s_top].num);
+                            s_top--;  
+                        }  
+                        num = calculate(out, out_top, stack, s_top);  //TODO
+                        printf("Print: %lld\n",num);
+                    } 
                     break;    
                 default:
                     break;    
-            }    
+            } 
+        }   
     }
     //postfix to cal
  
